cd380eb1df4776943881ffbb15f4814cbbd39be6
Durga909 <dmusinada@MacBook-Pro.local>
2020-06-28 17:04:17 -0400

    changed the name of the app name

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hey This is KRISHANTH LEKHASHREE PUTTY PATTIII.....
+          Hey This is LaddiLekhu LEKHASHREE PUTTY PATTIII app running on Kubernetes.....
         </a>
       </header>
       <div>
